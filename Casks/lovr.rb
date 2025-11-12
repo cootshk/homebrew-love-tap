05244108576bb3117270d2e9690b9439d57a9182
Henry <cootshk@duck.com>
2025-11-11 23:05:44 -0600

    fix(style) add final newlines

--- Casks/lovr.rb
+++ Casks/lovr.rb
@@ -3,10 +3,11 @@ cask "lovr" do
   sha256 "683b0ed094b689858e11d825c3a83994d61e66485dd9a071f501b53e656dbe77"
 
   url "https://github.com/bjornbytes/lovr/releases/download/v#{version}/lovr-v#{version}.app.zip",
-      verified: "github.com/bjornbytes/lovr"
+      verified: "github.com/bjornbytes/lovr/"
   name "LOVR"
-  desc "An open source Lua framework for building 3D games and VR experiences."
+  desc "Open source Lua framework for building 3D games and VR experiences"
   homepage "https://lovr.org/"
+
   app "lovr.app"
   binary "#{appdir}/lovr.app/Contents/MacOS/lovr"
 
